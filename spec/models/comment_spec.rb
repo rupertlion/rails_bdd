9855e14d9fc8b14d9d075578c0b12f31d594c492
rupertlion <rupertlion@gmail.com>
2018-09-22 15:37:24 +0200

    Added unit tests to validate format of email entry

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,13 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  describe 'DB table' do
-    it { is_expected.to have_db_column :views }
-    it { is_expected.to have_db_column :email }
-end
+    describe 'DB table' do
+        it { is_expected.to have_db_column :views }
+        it { is_expected.to have_db_column :email }
+    end
 
     describe 'Validations' do
         it { is_expected.to validate_presence_of :views}
+    end
+
+    describe 'Email validation' do
+        it { should allow_value('test@test.com').for(:email )}
+        it { should_not allow_value("test").for(:email) }
     end
 
     describe 'Factory' do

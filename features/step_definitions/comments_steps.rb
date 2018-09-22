162353bd558ff3889cbc0e76e4036cd5b3ef0761
rupertlion <rupertlion@gmail.com>
2018-09-22 14:59:48 +0200

    Deleted comment_specs as no additional steps required

--- features/step_definitions/comments_steps.rb
+++
@@ -1,4 +0,0 @@
-Given("the following persons view exists, with associated email address") do |table|
-    # table is a Cucumber::MultilineArgument::DataTable
-    pending # Write code here that turns the phrase above into concrete actions
-  end

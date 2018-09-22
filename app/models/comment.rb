9d65407938ed5d91cc576b03ce3cab401c3c849d
danmalmx <danmalmx@gmail.com>
2018-09-22 12:21:18 +0200

    passing email validation test

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :article
     validates :body, presence: true
-    validates_format_of :commenter,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+    validates :commenter, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }, allow_blank: true
 end
